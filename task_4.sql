1cb8197d08cbdff8f4fe41d8c9790c05b199da45
Muturi1999 <mikemuturi1999@gmail.com>
2025-02-01 21:48:48 +0300

    task-4

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,5 @@
 -- task_4.sql
 
--- Connect to the MySQL server and select the alx_book_store database
--- The database name is passed as an argument to the mysql command
-
 -- Query to retrieve the full description of the 'books' table
 SELECT 
     COLUMN_NAME AS 'Field',
@@ -12,7 +9,24 @@ SELECT
     COLUMN_DEFAULT AS 'Default',
     EXTRA AS 'Extra'
 FROM 
-    information_schema.COLUMNS
+    INFORMATION_SCHEMA.COLUMNS
 WHERE 
     TABLE_SCHEMA = 'alx_book_store' 
     AND TABLE_NAME = 'books';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
